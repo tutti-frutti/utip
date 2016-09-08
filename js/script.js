66c926dd5730885316f7d30936417230f4ce8e5d
Anton <anton.verstka@gmail.com>
2016-09-08 03:17:41 +0300

    work on mobile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
-    $(".nav__lang").click(function(event){
+    $(".nav__lang a").click(function(event){
         event.preventDefault();
-        $(".nav__sub").css("display", "block");
+        //$(".nav__sub").css("display", "block");
+        $(".nav__sub").slideToggle(200);
+        $(this).toggleClass('active');
     });
+    
+    $(".open-menu").click(function(event){
+        event.preventDefault();
+        $(".toogle-menu").show("500");
+        $(".close-menu").click(function(event){
+            event.preventDefault;
+            $(".toogle-menu").hide("500");
+        });
+    });
+    
+    $('.nav__sub-link').on('click',function(e){
+        var lang = $(this).attr('data-lang');
+        console.log(lang)
+        $('.lang_show').text(lang);
+        $('.lang_show').removeClass('active');
+        $(".nav__sub").slideUp();
+        e.preventDefault();
+    });
+    
 });
 
 $(function(){
@@ -11,14 +32,8 @@ $(function(){
         });
 	});
 
-$(function(){
-		$('#menu-2').slicknav({
-            duration: 500,
-        });
-	});
-
 $('.timeline').timelify();
 
 $( function() {
     $( "#accordion" ).accordion();
-  } );
+  });
