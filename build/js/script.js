4407bd7e14f4d687f9b911657b34af01cf07770b
Anton <anton.verstka@gmail.com>
2016-09-15 22:40:13 +0300

    work

---
+++ build/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $(".nav__lang a").click(function(event){
+        event.preventDefault();
+        //$(".nav__sub").css("display", "block");
+        $(".nav__sub").slideToggle(200);
+        $(this).toggleClass('active');
+    });
+    
+    $(".footer__lang a").click(function(event){
+        event.preventDefault();
+        $(".footer__sub").slideToggle(200);
+        $(this).toggleClass('active');
+    });
+    
+    $(".footer__sub-link").on('click', function(e){
+        var bottomlang = $(this).attr('data-speech');
+        $(".drop_lang").text(bottomlang);
+        $(".drop_lang").removeClass('active');
+        $(".footer__sub").slideUp();
+        e.preventDefault();
+    });
+    
+    $(".open-menu").click(function(event){
+        event.preventDefault();
+        $(".toogle-menu").show("500");
+        $(".close-menu").click(function(event){
+            event.preventDefault;
+            $(".toogle-menu").hide("500");
+        });
+    });
+    
+    $('.nav__sub-link').on('click',function(e){
+        var lang = $(this).attr('data-lang');
+        console.log(lang)
+        $('.lang_show').text(lang);
+        $('.lang_show').removeClass('active');
+        $(".nav__sub").slideUp();
+        e.preventDefault();
+    });
+    $('.open-menu').click(function(){
+        console.log(true)
+    })
+});
+
+$(function(){
+		$('#menu').slicknav({
+            duration: 500,
+        });
+	});
+
+$('.timeline').timelify();
+
+$('select').selectric();
+
+
+$( function() {
+    $( "#accordion" ).accordion({
+        heightStyle: "content",
+    });
+  });
+
+
+//});
